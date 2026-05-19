a78037698268c5be5dc1617106b9a48a3b5377f6
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-05-19 10:34:27 -0300

    Create stations.ts

---
+++ data/stations.ts
@@ -0,0 +1,15 @@
+export const stations = [
+  {
+    id: 1,
+    name: "Estação Paulista",
+    status: "Disponível",
+    power: "22 kW",
+  },
+
+  {
+    id: 2,
+    name: "Shopping Eldorado",
+    status: "Ocupado",
+    power: "50 kW",
+  },
+];
