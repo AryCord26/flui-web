d5a9c763d0fee56635528e47489afeca8edc3b3e
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-05-19 10:34:49 -0300

    Create reviews.ts

---
+++ data/reviews.ts
@@ -0,0 +1,13 @@
+export const reviews = [
+  {
+    id: 1,
+    user: "Lucas Ferreira",
+    comment: "Excelente estação.",
+  },
+
+  {
+    id: 2,
+    user: "Amanda Silva",
+    comment: "Carregamento muito rápido.",
+  },
+];
